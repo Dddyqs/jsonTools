cb4f681811d1e6376d86c9e1030ee8999e2499ae
Dddyqs <Dddyqs@users.noreply.github.com>
2026-05-08 16:11:11 +0000

    refactor: 移除未使用的 jq 查询功能 - 简化代码

--- jsonTools4Utools/preload.js
+++ jsonTools4Utools/preload.js
@@ -1,4 +1,2 @@
 const yaml = require("./assets/js-yaml");
-const { raw: jaqRaw, json: jaqJson, version: jaqVersion } = require("./assets/jaq-index.js");
 window.yaml = yaml;
-window.jaqWasm = { raw: jaqRaw, json: jaqJson, version: jaqVersion };
